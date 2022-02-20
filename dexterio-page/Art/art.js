6a5167e98341cac73681531838c9dd4920898039
Vedant Kulkarni <57015094+vam1519@users.noreply.github.com>
2022-02-20 10:48:51 +0530

    Update art.js

--- dexterio-page/Art/art.js
+++ dexterio-page/Art/art.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Box, Container, Text } from "@chakra-ui/react";
-
+import InfiniteScroll from 'react-infinite-scroller';
 import { useState, UseEffect, useEffect } from 'react';
 import { Button, ButtonGroup } from '@chakra-ui/react';
 import axios, * as others from 'axios';
@@ -8,22 +8,35 @@ import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 
+const UnsplashImage = ({ url, key }) => (
+    <div className="image-item" key={key} >
+      <img src={url} />
+    </div>
+  );
+
 const Art = ()=>{
 
   
 
     const url='https://picsum.photos/v2/list?page=0&limit=100';
     const [images,setImages]=useState([]);  
-    const getImages = ()=>{
-        axios.get(url).then((r)=>{
-            setImages(r.data);
-        });
+    const [loaded, setIsLoaded] = React.useState(false);
+    const fetchImages = (count = 10) => {
+        const apiRoot = "https://api.unsplash.com";
+        const accessKey = "{AfbHrpKR5I940b0Nixq5fFrWj87FnX_x5qjUuwXZFXI}";
+    
+        axios
+          .get(`${apiRoot}/photos/random?client_id=AfbHrpKR5I940b0Nixq5fFrWj87FnX_x5qjUuwXZFXI&count=${count}`)
+          .then (res => {
+            setImages([...images, ...res.data]);
+            setIsLoaded(true);
+          });
     };
 
 
 
     useEffect(()=>{
-        getImages();
+       fetchImages();
     },[]);
 
 
@@ -35,13 +48,23 @@ const Art = ()=>{
         <div className="zoom" className='art'>
             {images.map((image)=>{
                 return (
-                    <LazyLoadImage
-                    effect='blur'
-                    src={image.download_url}
-                    alt={image.alt_description}
-                    key={image.id}
-                    placeholderSrc="Loading..."
-                    />  
+                    <InfiniteScroll
+                    dataLength={10}
+                    next={() => fetchImages(5)}
+                    hasMore={true}
+                    loader={
+                     <img
+                        src="https://res.cloudinary.com/chuloo/image/upload/v1550093026/scotch-logo-gif_jq4tgr.gif"
+                        alt="loading"
+                     />}
+                >
+                    <div className="image-grid" style={{ marginTop: "30px" }}>
+                       {loaded ? 
+                           images.map((image, index) => (
+                               <UnsplashImage url={image.urls.regular} key={index} />
+                           )): ""}
+                   </div>
+               </InfiniteScroll>
                     
 
                 )
